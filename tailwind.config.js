c67eed9f349462f8bc898e60484ecca65cc33bff
wmmeg <wmmegal@gmail.com>
2025-01-14 18:20:37 +0200

    feat: init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './src/**/*.{html,js}',
-    ],
+    content: ['./src/**/*.{html,js}'],
     theme: {
         container: {
             center: true,
@@ -10,21 +11,21 @@ module.exports = {
             screens: {
                 sm: '375px',
                 md: '575px',
-                lg: '767px',
-                xl: '1200px',
+                lg: '1200px',
+                xl: '1464px',
             },
         },
         colors: {
-            'primary': '#1752ca',
-            'black': '#16140f',
-            'white': '#FFFFFF',
+            primary: '#0e71b8',
+            black: '#1c1c1c',
+            white: '#FFFFFF',
         },
         extend: {
             fontSize: {
-                '4xl': ['40px', '1'],
+                '4xl': ['36px', '1.2'],
+                '3xl': ['32px', '1.2'],
             },
         },
     },
-    plugins: [],
+    plugins: [forms, typography],
 };
-
